8ed9f1193313872511ab75c845c517ec9f448d2a
Max <hopsmax101@gmail.com>
2023-01-18 00:00:31 -0800

    add print

--- betterloxd.py
+++ betterloxd.py
@@ -30,8 +30,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 client = discord.Client()
-os.listdir("/git_repo")
-os.listdir("/git_repo/data")
+print(os.listdir("/git_repo"))
+print(os.listdir("/git_repo/data"))
 
 token = yaml.load(open('/git_repo/data/token.yaml'))['token']
 prefix = 'z!'
