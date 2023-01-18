5826605c34107f725a0d96ad5c717e29f6cac661
Max <hopsmax101@gmail.com>
2023-01-17 23:10:24 -0800

    remove loop

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #* This script runs our bot 
 #TODO make script run error-handling script in case the process goes down for whatever reason
-pwd
-ls 
-while true; do sleep 1000; done
+# pwd
+# ls 
+# while true; do sleep 1000; done
 python3 $1/betterloxd.py
