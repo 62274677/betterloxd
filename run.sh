fc950b8fc948c5f2aef580db791d3644fb2e283c
Max <hopsmax101@gmail.com>
2023-01-17 23:05:44 -0800

    add loop to runsh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 #* This script runs our bot 
 #TODO make script run error-handling script in case the process goes down for whatever reason
 pwd
-ls
+ls 
+while true; do sleep 1000; done
 python3 $1/betterloxd.py
