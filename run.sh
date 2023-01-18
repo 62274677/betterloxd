be051b6beb12c062d44537fd84c3aebea1d50469
Max <hopsmax101@gmail.com>
2023-01-18 02:10:56 -0800

    runsh

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 #TODO make script run error-handling script in case the process goes down for whatever reason
 # pwd
 # ls 
-while true; do sleep 1000; done
-python3 $1/betterloxd.py 
+# while true; do sleep 1000; done
+git clone $1
+pip install -r $2/requirements.txt
+
+python3 $2/betterloxd.py 
